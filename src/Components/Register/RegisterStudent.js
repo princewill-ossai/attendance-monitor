6fc59a7523efcb27f9655b2d1f46d0dc210393d7
Princewill-ossai <princewillossai@gmail.com>
2025-10-09 12:53:18 -0700

    Multiple selection canceled

--- src/Components/Register/RegisterStudent.js
+++ src/Components/Register/RegisterStudent.js
@@ -157,7 +157,6 @@ const Register = () => {
             onChange={handleChange}
             className="w-full border dark:bg-transparent dark:border-gray-900 rounded px-3 py-2 mb-4"
           >
-            <option value="" selected disabled>--select course(s)--</option>
             {
               courses.map(course => (
                 <option className="font-bold" value={course.id}>{`${course.code} - ${course.name}`}</option>
